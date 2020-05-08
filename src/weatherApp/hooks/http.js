29a2f6f1bebcc1c537d8febb2739b836d8b0af48
saifsamir-k <59330324+saifsamir-k@users.noreply.github.com>
2020-05-08 18:59:13 +0200

    Some modifications

--- src/weatherApp/hooks/http.js
+++ src/weatherApp/hooks/http.js
@@ -8,36 +8,34 @@ export const useHttp = (baseURL, dependancies, isSubmit) => {
     const [isLoading, setLoading] = useState(false);
 
     // Initialize the fetched data to an empty string
-    const [fetchedData, setFetchedData] = useState('');
+    const [fetchedData, setFetchedData] = useState(null);
 
     useEffect(() => {
         /*Check if isSubmit is true before fetching the corresponding 
         data*/
-        if (isSubmit) {
 
-            // set isLoading to true until we get the data
-            setLoading(true);
+        // set isLoading to true until we get the data
+        setLoading(true);
 
-            // Start fetching the data from the url received
-            fetch(baseURL)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Failed to fetch. ');
-                    }
-                    return response.json();
-                })
-                // Return the data when fetched successfully
-                .then(data => {
-                    setLoading(false);
-                    setFetchedData(data);
-                })
-                /*Show an alert when fetching encounters an error and stop the 
-                loader accordingly*/
-                .catch(err => {
-                    alert("Please insert a valid name")
-                    setLoading(false);
-                })
-        }
+        // Start fetching the data from the url received
+        fetch(baseURL)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch. ');
+                }
+                return response.json();
+            })
+            // Return the data when fetched successfully
+            .then(data => {
+                setLoading(false);
+                setFetchedData(data);
+            })
+            /*Show an alert when fetching encounters an error and stop the 
+            loader accordingly*/
+            .catch(err => {
+                alert("Please insert a valid name")
+                setLoading(false);
+            })
     }, dependancies)
     // Returning the data to use them later in displaying the weather
     return [isLoading, fetchedData];
